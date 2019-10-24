463a33d446d8d6f5e453b63bd1b4021ddeab2828
Ricardo Andres Moreno <octopus@MacBook-Pro-de-Ricardo.local>
2019-10-24 16:48:25 -0500

    add recursive fetch dashboard

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <nav class="topnav">
-                <Link to="/"><button class="btn-borde">Home</button></Link>
+  render() {
+    return (
+      <nav className="topnav">
+        <Link to="/">
+          <button className="btn-borde">Home</button>
+        </Link>
 
-                <Link to="/about"><button class="btn-borde">Register</button></Link>
+        <Link to="/about">
+          <button className="btn-borde">Register</button>
+        </Link>
 
-                <Link to="/DashBoard"><button class="btn-borde">DashBoard</button></Link>
+        <Link to="/DashBoard">
+          <button className="btn-borde">DashBoard</button>
+        </Link>
 
-                <Link to="/Reports"><button class="btn-borde">Reports</button></Link>
-            </nav >
-        )
-    }
+        <Link to="/Reports">
+          <button className="btn-borde">Reports</button>
+        </Link>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
--- src/Screens/dashboard/dashboard.js
+++ src/Screens/dashboard/dashboard.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { Component } from "react";
 
-function DashBoard() {
-    return (
-        <h3>Dash Board file</h3>
+class DashBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      realTimeData: {},
+      isLoading: true
+    };
+  }
 
-    )
+  recursiveFetch = () => {
+    fetch("https://ioteamcyf2019.herokuapp.com/api/v1/sensors/realtime")
+      .then(res => res.json())
+      .then(data => {
+        const datares = Object.assign({}, data);
+        console.log(datares);
+        this.setState({
+          realTimeData: datares,
+          isLoading: false
+        });
+      })
+      .catch(err => {
+        this.setState({
+          isLoading: false,
+          err: err
+        });
+      });
+    setTimeout(this.recursiveFetch, 5000);
+  };
 
+  componentDidMount = () => {
+    this.recursiveFetch();
+  };
+
+  render() {
+    if (this.state.isLoading) {
+      return <span>Loading....</span>;
+    } else if (this.state.err) {
+      return <span> something went wrong </span>;
+    } else if (!this.state.isLoading) {
+      return (
+        <div>
+          <div className="App-container">Realtime data</div>
+
+          <ul>
+            <li>Gas sensor {this.state.realTimeData.gasSensor}</li>
+            <li>Temperature sensor {this.state.realTimeData.tempSensor}</li>
+            <li>Voltage sensor {this.state.realTimeData.potSensor}</li>
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default DashBoard;
