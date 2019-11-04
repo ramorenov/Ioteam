b26bf786af42b1a86302fc979973dd201c7df1c2
Ricardo Andres Moreno <octopus@MacBook-Pro-de-Ricardo.local>
2019-11-04 09:09:43 -0500

    add carousel

---
+++ src/Screens/home/carousel.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+class Carouselimg extends Component {
+  render() {
+    return (
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showThumbs={false}
+        showStatus={false}
+        transitionTime={350}
+        interval={5000}
+        width={600}
+        dinamicHeight={true}
+      >
+        <div>
+          <img
+            src="https://live.staticflickr.com/1877/44216935831_b0ce92bed6.jpg"
+            alt="iotimg"
+          />
+          {/* <p className="legend">Legend 1</p> */}
+        </div>
+        <div>
+          <img
+            src="https://live.staticflickr.com/1744/28690279138_d43987e40d.jpg"
+            alt="agricultureiotimg"
+          />
+        </div>
+        <div>
+          <img
+            src="https://live.staticflickr.com/711/32544018313_1f89ed7f05.jpg"
+            alt="fireimg"
+          />
+        </div>
+        <div>
+          <img
+            src=" https://live.staticflickr.com/4828/46671186092_01474629de.jpg"
+            alt="iotblueimg"
+          />
+        </div>
+      </Carousel>
+    );
+  }
+}
+
+export default Carouselimg;
--- src/Screens/home/home.js
+++ src/Screens/home/home.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import code from '../../img/ioteamlog.png'
-import '../../App.scss';
+import React from "react";
+//import code from "../../img/ioteamlog.png";
+import "../../App.scss";
+import Carouselimg from "./carousel";
 
 function Home() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>IOTEAM</h1>
-                <img src={code} className="App-logo" alt="logo" />
-            </header>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>IOTEAM</h2>
+        <div className="App-carousel">
+          <Carouselimg />
         </div>
-    );
+
+        {/* <img src={code} className="App-logo" alt="logo" />  */}
+      </header>
+    </div>
+  );
 }
 
 export default Home;
