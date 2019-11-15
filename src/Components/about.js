abd2ce8d22feffc8d75944d5e0c64a77a41a5895
Ricardo Andres Moreno <octopus@MacBook-Pro-de-Ricardo.local>
2019-11-15 16:31:49 -0500

    Final changes

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,31 +1,41 @@
 import React, { Component } from "react";
 import "../Components/about.scss";
-// import loginImg from "../img/logo_transparent.png";
 
 class About extends Component {
   render() {
     return (
       <React.Fragment>
         <nav>
-			<ul>
-				<li><a href="#">About us</a>
-				<div>
-					<ul>
-						<li class="titulo">Mision</li>
-						<li>Provide and develop technological solutions in the IOT field for the benefit of all. </li>
-					</ul>
-					<ul>
-						<li class="titulo">Vision</li>
-						<li>Through innovation become leaders in the development, advice and application of IOT technologies in all productive fields (monitoring and response tasks.)</li>
-					</ul>
-					<ul>
-						<li class="titulo">Objetive</li>
-						<li>Accompany, advise and develop technological tools according to the needs of our customers.</li>
-					</ul>
-				</div>
-				</li>
-			</ul>
-		</nav>
+          <ul>
+            <li>
+              <a href="#">About us</a>
+              <div>
+                <ul>
+                  <li className="titulo">Mision</li>
+                  <li>
+                    Provide and develop technological solutions in the IOT field
+                    for the benefit of all.{" "}
+                  </li>
+                </ul>
+                <ul>
+                  <li className="titulo">Vision</li>
+                  <li>
+                    Through innovation become leaders in the development, advice
+                    and application of IOT technologies in all productive fields
+                    (monitoring and response tasks.)
+                  </li>
+                </ul>
+                <ul>
+                  <li className="titulo">Objetive</li>
+                  <li>
+                    Accompany, advise and develop technological tools according
+                    to the needs of our customers.
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </nav>
         <div className="container"></div>
       </React.Fragment>
     );
